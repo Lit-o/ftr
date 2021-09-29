2f84de16d2a9a5940d50bc38ea9a468764164fa6
Litvinenko Boris <boris12lit@gmail.com>
2021-09-29 18:08:49 +0300

    add check VCS

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to rock and rolla!
+          Edit <code>src/App.js</code> and save to rock and rolla! But check VCS settings
         </p>
         <a href="https://en.wikipedia.org/wiki/Main_Page"
         target="_blank"
