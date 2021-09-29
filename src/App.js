48762c28e712ba68946dcd5fb23178b35a9bd74e
Litvinenko Boris <boris12lit@gmail.com>
2021-09-29 17:53:54 +0300

    add ftr base

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './assets/pictures/ftr.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to rock and rolla!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <a href="https://en.wikipedia.org/wiki/Main_Page"
+        target="_blank"
         >
-          Learn React
+          Learn some stuff!
         </a>
       </header>
     </div>
