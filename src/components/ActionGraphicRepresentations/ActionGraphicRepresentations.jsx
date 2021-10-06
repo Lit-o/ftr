eefd66791639e5901b6c240494b77d04e9d35784
Boris <boris12lit@gmail.com>
2021-10-06 18:32:37 +0300

    added training .map for Character.jsx

--- src/components/ActionGraphicRepresentations/ActionGraphicRepresentations.jsx
+++ src/components/ActionGraphicRepresentations/ActionGraphicRepresentations.jsx
@@ -5,9 +5,9 @@ const ActionGraphicRepresentations = () => {
         <div className={style.actionsWrapper}>
             <h1 className='visuallyHidden'>Action Graphic Representations </h1>
             {/*через state и props будем менять url картинки или блок*/}
-            <img src="http://www.weilard.com/version3/pixelart/weilard_pixelart_moonwalker.gif" alt=""/>
+            {/*<img src="http://www.weilard.com/version3/pixelart/weilard_pixelart_moonwalker.gif" alt=""/>*/}
             {/*<img src="https://cdn.dribbble.com/users/3518215/screenshots/6392718/tumblr_nwl8ddm8xo1sbycm1o1_500.gif" alt=""/>*/}
-            {/*<img src="https://steamuserimages-a.akamaihd.net/ugc/2438013375536940927/D370DBF7BFC83ED36F783F08A598FFF3E71A1D61/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false" alt=""/>*/}
+            <img src="https://steamuserimages-a.akamaihd.net/ugc/2438013375536940927/D370DBF7BFC83ED36F783F08A598FFF3E71A1D61/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false" alt=""/>
         </div>
     )
 }
--- src/components/GamerInterfaces/Character/Character.jsx
+++ src/components/GamerInterfaces/Character/Character.jsx
@@ -1,21 +1,44 @@
 import style from './Character.module.css'
 
-const Character = () => {
+
+const Character = (props) => {
+
+    let thoughtsData = [
+        {id: 1, thought: "I'm awake. Let's go!"},
+        {id: 2, thought: "Maybe do something?!!!!"},
+        {id: 3, thought: "I feel hungry. I should go to the grocery store or order a pizza"}
+    ]
+
+    // let thoughtsResult = thoughtsData.map((el) => {
+    //         return (
+    //             <p>{el.thought}</p>
+    //         )
+    //     }
+    // )
+    let thoughtsResult = thoughtsData.map((el) =>  <p id={el.id}>{el.thought}</p>)
+
     return (
         <div className={style.main}>
             <h2>Character</h2>
-            <div className={style.attributesSection}>
+
+            <div className={style.charAttributes}>
                 <h3>Character attributes</h3>
                 <p>Agility : 5 </p>
                 <p>Strength : 5 </p>
                 <p>Intelligence: 5</p>
                 <p>Luck : 5 </p>
             </div>
-            <div>
-                <h3>My thoughts:</h3>
-                <textarea name="" id="" cols="25" rows="4"></textarea>
-                <button>Add thought</button>
 
+
+            <div className={style.charThoughts}>
+                <h3>My thoughts:</h3>
+                <div>
+                    <textarea name="" id=""></textarea>
+                    <br/>
+                    <button>Add thought</button>
+                </div>
+                <br/>
+                {thoughtsResult}
             </div>
         </div>
     )
