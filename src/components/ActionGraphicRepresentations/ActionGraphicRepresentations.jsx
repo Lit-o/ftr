f68a0f07b5dda922e7c3d5965f9879e92f7afcb6
Boris <boris12lit@gmail.com>
2021-10-16 15:08:35 +0300

    added hidden warning

--- src/components/ActionGraphicRepresentations/ActionGraphicRepresentations.jsx
+++ src/components/ActionGraphicRepresentations/ActionGraphicRepresentations.jsx
@@ -8,7 +8,7 @@ const ActionGraphicRepresentations = () => {
             {/*<img src="http://www.weilard.com/version3/pixelart/weilard_pixelart_moonwalker.gif" alt=""/>*/}
             {/*<img src="https://cdn.dribbble.com/users/3518215/screenshots/6392718/tumblr_nwl8ddm8xo1sbycm1o1_500.gif" alt=""/>*/}
             <img src="https://steamuserimages-a.akamaihd.net/ugc/2438013375536940927/D370DBF7BFC83ED36F783F08A598FFF3E71A1D61/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false" alt=""/>
-            <div className={style.warning}>
+            <div className={`${style.warning} + visuallyHidden`}>
                 <p>WARNING! Graphic engine has been disabled by an unknown rebel group.</p>
                 <p>You will not see a graphical representation of your actions on display</p>
                 <p>Graphic module switched to emergency text mode.</p>
