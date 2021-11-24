33994081c288d357d4d234c466bab4d25ebffc4d
Boris <boris12lit@gmail.com>
2021-11-24 05:59:49 +0300

    added editMode useState to CharacterCharacteristics.jsx

--- src/components/GamerInterfaces/Character/CharacterCharacteristics/CharacterCharacteristics.jsx
+++ src/components/GamerInterfaces/Character/CharacterCharacteristics/CharacterCharacteristics.jsx
@@ -1,16 +1,18 @@
 import style from "./CharacterCharacteristics.module.css"
-import React from "react";
+import React, {useState} from "react";
 
 const CharacterCharacteristics = (props) => {
 
+    let [editMode, setEditModeHook] = useState(true);
 
     const aquaStyle = {
         fontWeight: 700,
         paddingTop: '10px',
+        paddingBottom: '10px',
     };
 
     return (
-        <div className={style.charAttributes}>
+        <div className={style.charCharacteristics}>
             <h3>Сharacteristics</h3>
             <p>Strength : {props.characteristicsData.strength} </p>
             <p>Agility : {props.characteristicsData.agility} </p>
@@ -18,17 +20,27 @@ const CharacterCharacteristics = (props) => {
             <p>Intelligence : {props.characteristicsData.intelligence} </p>
             <p>Charisma : {props.characteristicsData.charisma} </p>
 
-            {/*мне кажется изящное решение когда в стейте будет 0, !!0 переведет значение в false
+            {/*мне кажется изящное решение, когда в стейте будет 0, !!0 переведет значение в false
              и этот блок не будет отображаться*/}
-            {!!props.characteristicsData.freeCharPoints &&
-                <div>
+            {!!props.characteristicsData.freeCharPoints && !editMode &&
+                <div className={style.charFreePoints}>
                     <p style={aquaStyle}>Free Char Points : {props.characteristicsData.freeCharPoints} <strong
                         data-tooltip="What the world gave you at birth">?</strong></p>
-
+                    <button>Use points</button>
                 </div>
             }
+            {!!props.characteristicsData.freeCharPoints && editMode &&
+            <div className={style.charFreePoints}>
+                <p style={aquaStyle}>Free Char Points : {props.characteristicsData.freeCharPoints} <strong
+                    data-tooltip="What the world gave you at birth">?</strong></p>
+                <button>Apply</button>
+                <button>Reset</button>
+            </div>
+            }
+
         </div>
     )
 }
+
 
 export default CharacterCharacteristics;
