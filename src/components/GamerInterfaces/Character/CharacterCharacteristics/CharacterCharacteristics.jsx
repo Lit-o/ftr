4ecc60a1fccbd00ba474803e76c8282bfe0ab4e3
Boris <boris12lit@gmail.com>
2021-11-25 08:52:50 +0300

    added WOW style for character Characteristics and zeroed func

--- src/components/GamerInterfaces/Character/CharacterCharacteristics/CharacterCharacteristics.jsx
+++ src/components/GamerInterfaces/Character/CharacterCharacteristics/CharacterCharacteristics.jsx
@@ -3,30 +3,62 @@ import React, {useState} from "react";
 
 const CharacterCharacteristics = (props) => {
 
-    let [editMode, setEditModeHook] = useState(true);
+    let [editMode, setEditModeHook] = useState(false);
+    let [freeCharPoints, setFreeCharPoints] = useState(props.characteristicsData.freeCharPoints)
 
-    const aquaStyle = {
-        fontWeight: 700,
-        paddingTop: '10px',
-        paddingBottom: '10px',
-    };
+    const activateEditMode = () => {
+        setEditModeHook(true)
+    }
+
+    // let strength = (props.characteristicsData.strength).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
+
+    function zeroed(number) {
+        return (number).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
+    }
 
     return (
         <div className={style.charCharacteristics}>
             <h3>Сharacteristics</h3>
-            <p>Strength : {props.characteristicsData.strength} </p>
-            <p>Agility : {props.characteristicsData.agility} </p>
-            <p>Perception : {props.characteristicsData.perception} </p>
-            <p>Intelligence : {props.characteristicsData.intelligence} </p>
-            <p>Charisma : {props.characteristicsData.charisma} </p>
+            <ul className={style.charItems}>
+                <li>
+                    <div>
+                        <p>Strength </p><p>{zeroed(props.characteristicsData.strength)}</p>
+                    </div>
+                    { editMode && <button>+</button> }
+                </li>
+                <li>
+                    <div>
+                        <p>Agility </p><p>{zeroed(props.characteristicsData.agility)}</p>
+                    </div>
+                    { editMode && <button>+</button> }
+                </li>
+                <li>
+                    <div>
+                        <p>Perception </p><p>{zeroed(props.characteristicsData.perception)}</p>
+                    </div>
+                    { editMode && <button>+</button> }
+                </li>
+                <li>
+                    <div>
+                        <p>Intelligence </p><p>{zeroed(props.characteristicsData.intelligence)}</p>
+                    </div>
+                    { editMode && <button>+</button> }
+                </li>
+                <li>
+                    <div>
+                        <p>Charisma </p><p>{zeroed(props.characteristicsData.charisma)}</p>
+                    </div>
+                    { editMode && <button>+</button> }
+                </li>
 
+            </ul>
             {/*мне кажется изящное решение, когда в стейте будет 0, !!0 переведет значение в false
              и этот блок не будет отображаться*/}
             {!!props.characteristicsData.freeCharPoints && !editMode &&
                 <div className={style.charFreePoints}>
-                    <p style={aquaStyle}>Free Char Points : {props.characteristicsData.freeCharPoints} <strong
+                    <p style={aquaStyle}>Free Char Points : {freeCharPoints} <strong
                         data-tooltip="What the world gave you at birth">?</strong></p>
-                    <button>Use points</button>
+                    <button onClick={activateEditMode}>Use points</button>
                 </div>
             }
             {!!props.characteristicsData.freeCharPoints && editMode &&
@@ -41,6 +73,13 @@ const CharacterCharacteristics = (props) => {
         </div>
     )
 }
+
+
+const aquaStyle = {
+    fontWeight: 700,
+    paddingTop: '10px',
+    paddingBottom: '10px',
+};
 
 
 export default CharacterCharacteristics;
