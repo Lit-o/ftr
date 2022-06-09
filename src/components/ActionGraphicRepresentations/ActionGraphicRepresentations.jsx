7c38b856d578503dbe3a148a513e3ad1ddfc472c
LitvinenkoBD <boris12lit@gmail.com>
2022-06-09 16:07:04 +0300

    small touch

--- src/components/ActionGraphicRepresentations/ActionGraphicRepresentations.jsx
+++ src/components/ActionGraphicRepresentations/ActionGraphicRepresentations.jsx
@@ -10,7 +10,9 @@ const ActionGraphicRepresentations = () => {
             {/*<img src="https://cdn.dribbble.com/users/3518215/screenshots/6392718/tumblr_nwl8ddm8xo1sbycm1o1_500.gif" alt=""/>*/}
             {/*<img src="https://steamuserimages-a.akamaihd.net/ugc/2438013375536940927/D370DBF7BFC83ED36F783F08A598FFF3E71A1D61/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false" alt=""/>*/}
             <img src={gif} alt="city of future"/>
-            <div className={`${style.warning} + visuallyHidden`}>
+            {/* <div className={`${style.warning} + visuallyHidden`}> */}
+
+            <div className={`${style.warning}`}>
                 <p>WARNING! Graphic engine has been disabled by an unknown rebel group.</p>
                 <p>You will not see a graphical representation of your actions on display</p>
                 <p>Graphic module switched to emergency text mode.</p>
